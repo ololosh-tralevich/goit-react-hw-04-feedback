e13c284f6e6c9db489b76bdc226618100899eb2b
ololosh-tralevich <karpeko76@gmail.com>
2022-03-31 18:44:02 +0100

    upd

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,12 +1,10 @@
-import { memo } from 'react';
-
 import PropTypes from 'prop-types';
 
 const Notification = ({ message }) => {
   return <h2>{message}</h2>;
 };
 
-export default memo(Notification);
+export default Notification;
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
